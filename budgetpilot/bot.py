830d4f215440c13ee0bbac7cc17d2277b8a0d807
vlagir <antoniokukuaka@gmail.com>
2026-06-20 12:48:37 +0300

    Add files via upload

---
+++ budgetpilot/bot.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    if message.text == "/start":
+        await message.answer("Я запущен 💰 BudgetPilot работает!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
